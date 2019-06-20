183e4b1436c57b9f291e3602f5e12b23f9c61291
Robert Sutherland <robert83sutherland@gmail.com>
2019-06-20 18:15:30 +0900

    Computer wechseln.
    - home: URLs und Templates hinzugefügt.

--- kgisteam/home/views.py
+++ kgisteam/home/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+from django.urls import reverse
 
-# Create your views here.
+
+def home(request):
+    """The home page."""
+    context = {}
+    return render(request, 'home/base.html', context)
+
+
+def home_redirect(request):
+    """Redirects to the home page."""
+    context = {}
+    return redirect(request, reverse('home'), context)
