4828a46e18e2c7f83d9e4101605f5073811dc5b4
0p3r4t0r <robert83sutherland@gmail.com>
2019-07-03 18:34:34 +0900

    Installed apps, updated homepage.
    DONE
    Installed courses, syllabi and worksheets.
    Updated the homepage.
    Updated the hero to be touch-responsive.

--- kgisteam/kgisteam/settings.py
+++ kgisteam/kgisteam/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'courses.apps.CoursesConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -38,8 +39,10 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'home.apps.HomeConfig',
+    'syllabi.apps.SyllabiConfig',
     'users.apps.UsersConfig',
     'utils.apps.UtilsConfig',
+    'worksheets.apps.WorksheetsConfig',
 ]
 
 MIDDLEWARE = [
