2e6736db8cc4c492ef3a44b5bd674ef62e0e2d05
Robert Sutherland <robert83sutherland@gmail.com>
2019-07-04 16:48:12 +0900

    Started to think about models.
    What kind of data do I need to define a course?

--- kgisteam/courses/models.py
+++ kgisteam/courses/models.py
@@ -1,16 +1,45 @@
 from django.db import models
 
+"""
+
+"""
+
 
 class Course(models.Model):
     """Store course information.
     
     course_name --> You should be careful about support for Japanese.
+    course_school --> Middle School or High School (MS or HS)
+    course_grade --> MS and HS: 1-3
+    course_class --> MS: A-E and HS: 1-8
+
+
     course_image --> an optional field to add and image for your course.
-    course_grade --> choose ms/hs and year.
-    course_class --> select from a list of possible classes. Make this easy to change.
     
     course_syllabus --> relation? separate app?
     course_worksheets --> relation? separate app?
     """
+    MIDDLESCHOOL = 'MS'
+    HIGHSCHOOL = 'HS'    
 
-    pass
+    GRADE_CHOICES = [
+        (MIDDLESCHOOL, 'Middle School'),
+        (HIGHSCHOOL, 'High School'),
+    ]
+
+
+    course_name = models.CharField(
+        max_length=30
+        )
+    course_school = models.CharField(
+        max_length=2,
+        choices=GRADE_CHOICES,
+        )
+    course_grade = models.IntegerField(
+        max_length=1,
+        # validator that only allows 1-3.
+        )
+    course_class = models.CharField(
+        max_length=1,
+        # validator that only allows A-E or 1-8
+        )
