18cd843026c437a988e11925bf00e5b3512a10cc
0p3r4t0r <robert83sutherland@gmail.com>
2019-08-30 23:49:40 -0500

    Removed extra content from the info page.

--- kgisteam/info/views.py
+++ kgisteam/info/views.py
@@ -5,15 +5,10 @@ from django.shortcuts import redirect, render
 from django.urls import reverse
 
 
-# App metadata
-courses_config = apps.get_app_config('courses')
-
-
 def info_home(request):
     """The info page."""
     context = {
-        'app_configs': (courses_config),
-        'steam_acronym': ('Keyless Grading Interface', 'Science', 'Technology', 'Engineering', 
-                            'Art', 'Mathematics',),
+        'steam_acronym': ('Keyless', 'Grading', 'Interface',
+            'Science', 'Technology', 'Engineering', 'Art', 'Mathematics',),
     }
     return render(request, 'info/info_home.html', context)
