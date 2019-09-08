acc12ec1481ddc93b281aa0d53fe70f10222b8c3
0p3r4t0r <robert83sutherland@gmail.com>
2019-09-09 08:57:45 +0900

    Properly render scientific notation in LaTeX.

--- kgisteam/courses/utils.py
+++ kgisteam/courses/utils.py
@@ -1,3 +1,12 @@
-def sn_round(number):
+def sn_round(number: float) -> float:
     """Round with scientific notation."""
     return eval('{:.2e}'.format(number))
+
+def sn_round_str(number: float) -> str:
+    """Round with scientific notation."""
+    rounded = '{:.2e}'.format(number)
+    value, order = rounded.split('e')
+    if order.startswith('+'):
+        order = order[1:].lstrip('0')
+    order = '{' + order + '}'
+    return '{value} \\times 10^{order}'.format(value=value, order=order)
